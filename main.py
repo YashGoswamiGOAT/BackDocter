cfb9993273ee5cda02b87dc6302927381e8763f9
yashg <yashgoswamigoat@gmail.com>
2023-07-28 15:13:29 +0530

    Patch v1.6

--- main.py
+++ main.py
@@ -10,13 +10,6 @@ from newsapi import NewsApiClient
 import random
 
 newsapi = NewsApiClient(api_key="aa9b869592d846948528369252d4cd54")
-dataframe = pd.read_csv('data.csv')
-
-def filter(col,text):
-    return dataframe[col].astype(str).str.lower().astype(str).str.contains(text.lower())
-def Search(dataframe,text):
-    return dataframe[filter('HOSP_NAME',text) | filter('HOSP_TYPE',text) | filter('DISTRICT_NAME',text) | filter('SPECIALTY_NAME',text) | filter('HOSP_CONTACT_NO',text) | filter('HOSP_EMAIL_ID',text) | filter('HOSP_ADDRESS',text)]
-
 
 cred = credentials.Certificate("drnearme.json")
 firebase_admin.initialize_app(cred,{
